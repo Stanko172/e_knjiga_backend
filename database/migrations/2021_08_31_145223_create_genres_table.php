fc2e55ad2a242b30dd8c8f23d7ff2108e44bb1f7
Igor Vukic <igor.vukic8877@gmail.com>
2021-08-31 15:03:58 +0200

    added genre migration

--- database/migrations/2021_08_31_145223_create_genres_table.php
+++ database/migrations/2021_08_31_145223_create_genres_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBooksTable extends Migration
+class CreateGenresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateBooksTable extends Migration
      */
     public function up()
     {
-        Schema::create('books', function (Blueprint $table) {
+        Schema::create('genres', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->integer('amaunt');
-            $table->decimal('price', $precision = 8, $scale = 2);
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ class CreateBooksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('books');
+        Schema::dropIfExists('genres');
     }
 }
--- database/migrations/2021_08_31_145546_create_book_genres_table.php
+++ database/migrations/2021_08_31_145546_create_book_genres_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateBooksTable extends Migration
+class CreateBookGenresTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateBooksTable extends Migration
      */
     public function up()
     {
-        Schema::create('books', function (Blueprint $table) {
+        Schema::create('book_genres', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->integer('amaunt');
-            $table->decimal('price', $precision = 8, $scale = 2);
+            $table->foreignId('book_id')->constrained();
+            $table->foreignId('genre_id')->constrained();
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreateBooksTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('books');
+        Schema::dropIfExists('book_genres');
     }
 }
